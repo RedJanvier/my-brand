12cfd1837b886977e9f84395fb927e7e9b492d6c
RedJanvier <janvierntwali@gmail.com>
2020-08-19 10:15:19 +0200

    feat(portfolio): make horizontal slide
    - show single portfolio project each 5 seconds
    - make responsive the slider

---
+++ UI/js/index.js
@@ -0,0 +1,23 @@
+let slide = 1;
+showSlides(slide);
+setInterval(() => changeSlide(1), 5000);
+function changeSlide(n) {
+  showSlides((slide += n));
+}
+function currentSlide(n) {
+  showSlides((slide = n));
+}
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {
+    slide = 1;
+  }
+  if (n < 1) {
+    slide = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slide - 1].style.display = "flex";
+}
