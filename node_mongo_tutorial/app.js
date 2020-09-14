030aee997f7a73059f0128c25d03aa712e7e9f78
RedJanvier <janvierntwali@gmail.com>
2020-09-14 10:29:34 +0200

    feat(node): setup tutorial server for posts crud

---
+++ node_mongo_tutorial/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+// Connect to MongoDB database
+mongoose
+  .connect("mongodb://localhost:27017/acmedb", { useNewUrlParser: true })
+  .then(() => {
+    const app = express();
+
+    app.use(express.json());
+    app.use("/api", routes);
+
+    app.listen(5000, () => {
+      console.log("Server has started!");
+    });
+  });
---
+++ node_mongo_tutorial/routes.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const Post = require("./models/Post");
+const router = express.Router();
+
+// Get all posts
+router.get("/posts", async (req, res) => {
+  const posts = await Post.find();
+  res.send(posts);
+});
+
+// Get Single Post
+router.get("/posts/:id", async (req, res) => {
+  try {
+    const post = await Post.findOne({ _id: req.params.id });
+    res.send(post);
+  } catch {
+    res.status(404);
+    res.send({ error: "Post doesn't exist!" });
+  }
+});
+
+// Create Post
+router.post("/posts", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  await post.save();
+  res.send(post);
+});
+
+// Update Post
+router.patch("/posts/:id", async (req, res) => {
+  try {
+    const post = await Post.findOne({ _id: req.params.id });
+
+    if (req.body.title) {
+      post.title = req.body.title;
+    }
+
+    if (req.body.content) {
+      post.content = req.body.content;
+    }
+
+    await post.save();
+    res.send(post);
+  } catch {
+    res.status(404);
+    res.send({ error: "Post doesn't exist!" });
+  }
+});
+
+// Delete Post
+router.delete("/posts/:id", async (req, res) => {
+  try {
+    await Post.deleteOne({ _id: req.params.id });
+    res.status(204).send();
+  } catch {
+    res.status(404);
+    res.send({ error: "Post doesn't exist!" });
+  }
+});
+
+module.exports = router;
