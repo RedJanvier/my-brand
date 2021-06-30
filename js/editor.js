66db787b5917212f27acf1a2f2ebe87bcb87f303
RedJanvier <janvierntwali@gmail.com>
2021-06-30 08:05:13 +0200

    feat(blog): add code snippets support

--- js/editor.js
+++ js/editor.js
@@ -1,3 +1,18 @@
-ClassicEditor.create(document.querySelector("#editor")).catch((error) => {
+ClassicEditor.create(document.querySelector('#editor'), {
+  toolbar: {
+    items: ['heading', 'bold', 'italic', 'link', 'codeBlock', 'table', 'undo'],
+  },
+  codeBlock: {
+    languages: [
+      { language: 'javascript', label: 'JavaScript' },
+      { language: 'typescript', label: 'TypeScript' },
+      { language: 'c', label: 'C' },
+      { language: 'cpp', label: 'C++' },
+      { language: 'css', label: 'CSS' },
+      { language: 'html', label: 'HTML' },
+      { language: 'java', label: 'Java' },
+    ],
+  },
+}).catch((error) => {
   console.error(error);
 });
